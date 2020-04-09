189a0110b397799204294a7c2272f6ed03d37d0b
lihaiyong-sys/community <15239423098@163.com>
2020-04-09 14:01:16 +0800

    init repo

---
+++ src/main/java/lift/majiang/communlty/communlty/CommunltyApplication.java
@@ -0,0 +1,14 @@
+package lift.majiang.communlty.communlty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunltyApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CommunltyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lift/majiang/communlty/communlty/CommunltyApplicationTests.java
@@ -0,0 +1,13 @@
+package lift.majiang.communlty.communlty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunltyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
