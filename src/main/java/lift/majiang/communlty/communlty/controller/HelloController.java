57cc14bb5398bb4c40355040433573d17c9590b4
lihaiyong-sys/community <15239423098@163.com>
2020-04-09 15:45:49 +0800

    add index and bootstrap

--- src/main/java/lift/majiang/communlty/communlty/controller/HelloController.java
+++ src/main/java/lift/majiang/communlty/communlty/controller/HelloController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model){
         model.addAttribute("name",name);
-        return "hello";
+        return "index";
     }
 }
